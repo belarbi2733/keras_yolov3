f84e21d2dcf8326c7ee7aa388ef4fe2c4216aa00
Mohammed Amin Belarbi <mohammedamin.belarbi@umons.ac.be>
2019-09-25 15:20:38 +0200

    new commit

---
+++ test_yolo.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+import argparse
+from yolo import YOLO, detect_video
+from PIL import Image
+
+
+def detect_img(yolo, image_path, output_path=''):
+    try:
+        image = Image.open(image_path)
+    except:
+        print ('Open Error! Try again!')
+    r_image = yolo.detect_image(image)
+    r_image.save(output_path)
+    yolo.close_session()
+
+
+FLAGS = None
+
+if __name__ == '__main__':
+
+    # class YOLO defines the default value, so suppress any default here
+
+    parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
+    parser.add_argument('--model', type=str,
+                        help='path to model weight file, default '
+                        + YOLO.get_defaults('model_path'))
+
+    parser.add_argument('--anchors', type=str,
+                        help='path to anchor definitions, default '
+                        + YOLO.get_defaults('anchors_path'))
+
+    parser.add_argument('--classes', type=str,
+                        help='path to class definitions, default '
+                        + YOLO.get_defaults('classes_path'))
+
+    parser.add_argument('--gpu_num', type=int,
+                        help='Number of GPU to use, default '
+                        + str(YOLO.get_defaults('gpu_num')))
+
+    parser.add_argument('--image', default=False, action='store_true',
+                        help='Image detection mode, will ignore all positional arguments'
+                        )
+    parser.add_argument('--video', default=False, action='store_true',
+                        help='video detection mode, will ignore all positional arguments'
+                        )
+    parser.add_argument('--input', nargs='?', type=str,
+                        help='Video or image input path')
+
+    parser.add_argument('--output', nargs='?', type=str, default='',
+                        help='[Optional] Image or Video output path')
+
+    FLAGS = parser.parse_args()
+
+    if FLAGS.image:
+        print ('Image detection mode')
+        if 'input' in FLAGS:
+            detect_img(YOLO(**vars(FLAGS)), FLAGS.input, FLAGS.output)
+        else:
+            print ('Must specify at least image_input_path.  See usage with --help.')
+    elif FLAGS.video:
+        if 'input' in FLAGS:
+            detect_video(YOLO(**vars(FLAGS)), FLAGS.input, FLAGS.output)
+        else:
+            print ('Must specify at least video_input_path.  See usage with --help.')
